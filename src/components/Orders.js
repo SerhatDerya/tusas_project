6b211a03abc87318f441411a7a43debc01718a54
serhat_derya <88051030+SerhatDerya@users.noreply.github.com>
2023-12-22 15:58:05 +0300

    Create Orders.js

---
+++ src/components/Orders.js
@@ -0,0 +1,55 @@
+import Order from './Order';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { Button } from 'primereact/button';
+import React, { useRef } from 'react';
+import { OverlayPanel } from 'primereact/overlaypanel';
+import { InputText } from 'primereact/inputtext';
+import { InputNumber } from 'primereact/inputnumber';
+import { useState } from "react";
+import EditOrder from './EditOrder';
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import AddOrder from './AddOrder';
+
+function Orders({ orders, onDelete, onEdit }){
+
+    
+
+    const renderedOrders = orders.map((order) => {
+        return <Order onEdit={onEdit} onDelete={onDelete} key={order.id} order={order} />
+    });
+
+    const textEditor = (options) => {
+        return <InputText type="text" value={options.value} onChange={(e) => options.editorCallback(e.target.value)} />;
+    };
+    
+    const onRowEditComplete = (e) => {
+        onEdit(e);
+    };
+    
+    const BodyTemplate = (rowData) => {
+        return (
+            <React.Fragment>
+                <Button icon="pi pi-trash" rounded outlined severity="danger" onClick={() => onDelete(rowData)} />
+            </React.Fragment>
+        );
+    };
+
+
+    return (
+    <div className='card'>  
+        <DataTable value={orders} stripedRows editMode="row" dataKey="id" onRowEditComplete={onRowEditComplete}  tableStyle={{ minWidth: '50rem' }}>
+                <Column  field="id" header="Id"></Column>
+                <Column field="ucak" header="Uçak"  editor={(options) => textEditor(options)}></Column>
+                <Column field="musteri" header="Müşteri" editor={(options) => textEditor(options)}></Column>
+                <Column field='tarih' header="Üretim Tarihi" dataType="date"></Column>
+                <Column rowEditor headerStyle={{ width: '10%', minWidth: '8rem' }} bodyStyle={{ textAlign: 'center' }}></Column>
+                <Column body={BodyTemplate}></Column>
+        </DataTable>
+    </div>)
+
+}
+
+export default Orders;
