534a4466400e1de558a264c9e1f33364456e2f23
serhat_derya <88051030+SerhatDerya@users.noreply.github.com>
2023-12-28 18:11:28 +0300

    Update Orders.js

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -3,27 +3,31 @@ import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
 import React, { useRef } from 'react';
-import { OverlayPanel } from 'primereact/overlaypanel';
 import { InputText } from 'primereact/inputtext';
-import { InputNumber } from 'primereact/inputnumber';
+import { Calendar } from 'primereact/calendar';
 import { useState } from "react";
-import EditOrder from './EditOrder';
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-import AddOrder from './AddOrder';
+import { Dropdown } from 'primereact/dropdown';
 
 function Orders({ orders, onDelete, onEdit }){
 
-    
-
-    const renderedOrders = orders.map((order) => {
+    /* const renderedOrders = orders.map((order) => {
         return <Order onEdit={onEdit} onDelete={onDelete} key={order.id} order={order} />
-    });
+    }); */
 
     const textEditor = (options) => {
         return <InputText type="text" value={options.value} onChange={(e) => options.editorCallback(e.target.value)} />;
     };
+
+    const dateEditor = (options) => {
+        return <Calendar onChange={(e) => options.editorCallback(e.target.value.getDate()+"/"+(e.target.value.getMonth()+1)+"/"+e.target.value.getFullYear())} dateFormat="dd/mm/yy" />
+    }
+
+    const drowdownEditor = (options) => {
+        return <Dropdown value={options.value} options={["Aksungur","Anka","Gökbey","Hürjet"]} onChange={(e) => options.editorCallback(e.target.value)} />
+    }
     
     const onRowEditComplete = (e) => {
         onEdit(e);
@@ -39,16 +43,17 @@ function Orders({ orders, onDelete, onEdit }){
 
 
     return (
-    <div className='card'>  
+    <div className='card'> 
         <DataTable value={orders} stripedRows editMode="row" dataKey="id" onRowEditComplete={onRowEditComplete}  tableStyle={{ minWidth: '50rem' }}>
                 <Column  field="id" header="Id"></Column>
-                <Column field="ucak" header="Uçak"  editor={(options) => textEditor(options)}></Column>
+                <Column field="ucak" header="Uçak"  editor={(options) => drowdownEditor(options)}></Column>
                 <Column field="musteri" header="Müşteri" editor={(options) => textEditor(options)}></Column>
-                <Column field='tarih' header="Üretim Tarihi" dataType="date"></Column>
+                <Column field='tarih' header="Üretim Tarihi" editor={(options) => dateEditor(options)}></Column>
                 <Column rowEditor headerStyle={{ width: '10%', minWidth: '8rem' }} bodyStyle={{ textAlign: 'center' }}></Column>
                 <Column body={BodyTemplate}></Column>
         </DataTable>
-    </div>)
+    </div>
+    )
 
 }
 
