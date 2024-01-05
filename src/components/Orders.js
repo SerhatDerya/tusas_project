65f54276b92c41d021b64bc1a8bc35ac167ba52a
serhat_derya <88051030+SerhatDerya@users.noreply.github.com>
2024-01-05 15:38:57 +0300

    Update Orders.js

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,17 +1,17 @@
-import Order from './Order';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
-import React, { useRef } from 'react';
+import React from 'react';
 import { InputText } from 'primereact/inputtext';
 import { Calendar } from 'primereact/calendar';
-import { useState } from "react";
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import { Dropdown } from 'primereact/dropdown';
+import AddOrder from './AddOrder';
 
-function Orders({ orders, onDelete, onEdit }){
+
+function Orders({ orders, onDelete, onEdit, onCreate }){
 
     /* const renderedOrders = orders.map((order) => {
         return <Order onEdit={onEdit} onDelete={onDelete} key={order.id} order={order} />
@@ -26,7 +26,8 @@ function Orders({ orders, onDelete, onEdit }){
         return <Calendar value={new Date(options.value)} onChange={(e) => options.editorCallback(e.target.value.getDate()+"/"+(e.target.value.getMonth()+1)+"/"+e.target.value.getFullYear())} dateFormat="dd/mm/yy" />
     }
 
-    const drowdownEditor = (options) => {
+    const dropdownEditor = (options) => {
+        
         return <Dropdown value={options.value} options={["Aksungur","Anka","Gökbey","Hürjet"]} onChange={(e) => options.editorCallback(e.target.value)} />
     }
     
@@ -44,10 +45,12 @@ function Orders({ orders, onDelete, onEdit }){
 
 
     return (
-    <div className='card'> 
+    <div className='card'>
+        <AddOrder onCreate={onCreate} />
         <DataTable value={orders} stripedRows editMode="row" dataKey="id" onRowEditComplete={onRowEditComplete}  tableStyle={{ minWidth: '50rem' }}>
                 <Column  field="id" header="Id"></Column>
-                <Column field="ucak" header="Uçak"  editor={(options) => drowdownEditor(options)}></Column>
+                <Column field="ucakKodu" header="Uçak Adı"  editor={(options) => textEditor(options)}></Column>
+                <Column field="ucak" header="Uçak Tipi"  editor={(options) => dropdownEditor(options)}></Column>
                 <Column field="musteri" header="Müşteri" editor={(options) => textEditor(options)}></Column>
                 <Column field='tarih' header="Üretim Tarihi" editor={(options) => dateEditor(options)}></Column>
                 <Column rowEditor headerStyle={{ width: '10%', minWidth: '8rem' }} bodyStyle={{ textAlign: 'center' }}></Column>
