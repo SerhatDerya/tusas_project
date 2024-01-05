77f6d2af29f7c63f155ebc3647c3992efdcd6f44
serhat_derya <88051030+SerhatDerya@users.noreply.github.com>
2024-01-05 15:39:46 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import { useEffect, useState } from "react";
 import Orders from './components/Orders';
 import AddOrder from './components/AddOrder';
+import Ucuslar from "./components/Ucuslar";
+import MainContent from "./components/MainContent";
 import axios from 'axios';
+import { TabMenu } from 'primereact/tabmenu';
+import React, { useRef } from 'react';
+import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
+import { Toast } from 'primereact/toast';
         
 
 function App(){
+    const toast = useRef(null);
+
     const [orders, setOrders] = useState([]);
-    const [ucak, setUcak] = useState();
-    const [musteri, setMusteri] = useState();
-    
+
+    const [activeIndex, setActiveIndex] = useState(3);
+
+    const menuItems = [
+        {label: 'Uçaklar', icon: 'pi pi-fw pi-send'},
+        {label: 'Uçuşlar', icon: 'pi pi-fw pi-calendar'},
+        {label: 'Admin Paneli', icon: 'pi pi-fw pi-user'},
+        {label: 'Ayarlar', icon: 'pi pi-fw pi-cog'}
+    ];
 
     const fetchOrders = async () => {
         const response = await axios.get('http://localhost:3001/orders');
@@ -18,6 +32,7 @@ function App(){
 
     useEffect(() => {
         fetchOrders();
+        setActiveIndex(0);
     }, []);
 
     const editOrder = async (e) => {
@@ -36,29 +51,56 @@ function App(){
 
         setOrders(updatedOrders); */
 
-        let _orders = [...orders];
-        let { newData, index } = e;
+        confirmDialog({
+            message: 'Uçak bilgilerini değiştirme istediğinize emin misiniz?',
+            header: 'Uyarı',
+            icon: 'pi pi-exclamation-triangle',
+            accept: async () => {
 
-        _orders[index] = newData;
+                let _orders = [...orders];
+                let { newData, index } = e;
 
-        setOrders(_orders);
+                _orders[index] = newData;
 
-        const response = await axios.put(`http://localhost:3001/orders/${newData.id}`, {
-            "id":newData.id,
-            "ucak":newData.ucak,
-            "musteri":newData.musteri,
-            "tarih": newData.tarih
+                setOrders(_orders);
+
+                await axios.put(`http://localhost:3001/orders/${newData.id}`, {
+                    "id":newData.id,
+                    "ucakKodu":newData.ucakKodu,
+                    "ucak":newData.ucak,
+                    "musteri":newData.musteri,
+                    "tarih": newData.tarih
+                });
+            
+                toast.current.show({ severity: 'success', summary: 'İşlem Tamamlandı', detail: 'Uçak bilgileri başarıyla değiştirildi', life: 3000 });
+            },
+            acceptLabel: 'Evet',
+            rejectLabel: 'Hayır'
         });
+
+        
+
     };
 
     const deleteOrder = async (rowData) => {
-        await axios.delete(`http://localhost:3001/orders/${rowData.id}`);
+        confirmDialog({
+            message: 'Uçak bilgilerini silmek istediğinize emin misiniz?',
+            header: 'Uyarı',
+            icon: 'pi pi-exclamation-triangle',
+            accept: async () => {
+                await axios.delete(`http://localhost:3001/orders/${rowData.id}`);
 
-        const updatedOrders = orders.filter((order) => {
-            return order.id !== rowData.id;
-        })
+                const updatedOrders = orders.filter((order) => {
+                    return order.id !== rowData.id;
+                })
 
-        setOrders(updatedOrders);
+                setOrders(updatedOrders);
+
+                toast.current.show({ severity: 'success', summary: 'İşlem Tamamlandı', detail: 'Uçak bilgileri başarıyla silindi', life: 3000 });
+            },
+            acceptLabel: 'Evet',
+            rejectLabel: 'Hayır'
+        });
     }
 
     const createOrder = async (ucak, musteri, tarih) => {
@@ -78,8 +120,12 @@ function App(){
     }
 
     return <div>
-        <AddOrder onCreate={createOrder} />
-        <Orders onEdit={editOrder} orders={orders} onDelete={deleteOrder} />
+        <Toast ref={toast} />
+        <ConfirmDialog />
+        <TabMenu model={menuItems} className="tabMenu" activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)}/>
+        <MainContent activeIndex={activeIndex} onEdit={editOrder} orders={orders} onDelete={deleteOrder} onCreate={createOrder} />
+        {/* <Orders onEdit={editOrder} orders={orders} onDelete={deleteOrder} />
+        <Ucuslar onEdit={editOrder} orders={orders} onDelete={deleteOrder} /> */}
     </div>
 }
 
