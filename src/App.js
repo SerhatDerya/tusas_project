d095fb1e5969f298b48daec1a4abfb31fdb26400
serhat_derya <88051030+SerhatDerya@users.noreply.github.com>
2024-01-25 22:34:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import { useEffect, useState } from "react";
-import Orders from './components/Orders';
-import AddOrder from './components/AddOrder';
-import Ucuslar from "./components/Ucuslar";
 import MainContent from "./components/MainContent";
 import axios from 'axios';
 import { TabMenu } from 'primereact/tabmenu';
@@ -14,8 +11,14 @@ function App(){
     const toast = useRef(null);
 
     const [orders, setOrders] = useState([]);
+    const [musteriler, setMusteriler] = useState([]);
+    const [musterilerTable, setMusterilerTable] = useState([]);
+    const [ucakTipleri, setUcakTipleri] = useState([]);
+    const [ucakTipleriTable, setUcakTipleriTable] = useState([]);
+    const [lokasyonlar, setLokasyonlar] = useState([]);
+    const [lokasyonlarTable, setLokasyonlarTable] = useState([]);
 
-    const [activeIndex, setActiveIndex] = useState(3);
+    const [activeIndex, setActiveIndex] = useState(0);
 
     const menuItems = [
         {label: 'Uçaklar', icon: 'pi pi-fw pi-send'},
@@ -24,16 +27,52 @@ function App(){
         {label: 'Ayarlar', icon: 'pi pi-fw pi-cog'}
     ];
 
-    const fetchOrders = async () => {
-        const response = await axios.get('http://localhost:3001/orders');
+    const fetchLokasyonlar = async () => {
+        const response = await axios.get("http://localhost:8081/lokasyonlar");
+        setLokasyonlarTable(response.data);
 
+        const arr = [];
+        for (let i = 0; i < response.data.length; i++) {
+            arr.push(response.data[i]["lokasyonAdi"]);
+        }
+        setLokasyonlar(arr);
+    };
+
+
+    const fetchUcakTipleri = async () => {
+        const response = await axios.get("http://localhost:8081/ucakTipleri");
+        setUcakTipleriTable(response.data);
+
+        const arr = [];
+        for (let i = 0; i < response.data.length; i++) {
+            arr.push(response.data[i]["ucakTipi"]);
+        }
+        setUcakTipleri(arr);
+    };
+
+    const fetchMusteriler = async () => {
+        const response = await axios.get("http://localhost:8081/musteriler");
+        setMusterilerTable(response.data);
+
+        const arr = [];
+        for (let i = 0; i < response.data.length; i++) {
+            arr.push(response.data[i]["musteriAdi"]);
+        }
+        setMusteriler(arr);
+    };
+
+    const fetchOrders = async () => {
+        const response = await axios.get('http://localhost:8081/orders');
+        console.log(response.data)
         setOrders(response.data);
     };
 
     useEffect(() => {
         fetchOrders();
-        setActiveIndex(0);
-    }, []);
+        fetchMusteriler();
+        fetchUcakTipleri();
+        fetchLokasyonlar();
+    }, [activeIndex]);
 
     const editOrder = async (e) => {
         /* const response = await axios.put(`http://localhost:3001/orders/${id}`, {
@@ -52,7 +91,7 @@ function App(){
         setOrders(updatedOrders); */
 
         confirmDialog({
-            message: 'Uçak bilgilerini değiştirme istediğinize emin misiniz?',
+            message: 'Uçak bilgilerini değiştirmek istediğinize emin misiniz?',
             header: 'Uyarı',
             icon: 'pi pi-exclamation-triangle',
             accept: async () => {
@@ -61,15 +100,17 @@ function App(){
                 let { newData, index } = e;
 
                 _orders[index] = newData;
-
+                console.log(newData);
                 setOrders(_orders);
 
-                await axios.put(`http://localhost:3001/orders/${newData.id}`, {
+
+                await axios.put(`http://localhost:8081/orders/${newData.id}`, {
                     "id":newData.id,
                     "ucakKodu":newData.ucakKodu,
                     "ucak":newData.ucak,
                     "musteri":newData.musteri,
-                    "tarih": newData.tarih
+                    "tarih": newData.tarih,
+                    "toplamUcusSuresi":newData.toplamUcusSuresi
                 });
             
                 toast.current.show({ severity: 'success', summary: 'İşlem Tamamlandı', detail: 'Uçak bilgileri başarıyla değiştirildi', life: 3000 });
@@ -88,7 +129,7 @@ function App(){
             header: 'Uyarı',
             icon: 'pi pi-exclamation-triangle',
             accept: async () => {
-                await axios.delete(`http://localhost:3001/orders/${rowData.id}`);
+                await axios.delete(`http://localhost:8081/orders/${rowData.id}`);
 
                 const updatedOrders = orders.filter((order) => {
                     return order.id !== rowData.id;
@@ -103,19 +144,21 @@ function App(){
         });
     }
 
-    const createOrder = async (ucak, musteri, tarih) => {
-        const response = await axios.post('http://localhost:3001/orders', {
+    const createOrder = async (ucak, ucakKodu, musteri, tarih) => {
+        const response = await axios.post('http://localhost:8081/orders', {
                 "id": Math.floor(Math.random()*9999),
                 "ucak":ucak,
+                "ucakKodu": ucakKodu,
                 "musteri":musteri,
-                "tarih":tarih
+                "tarih":tarih,
+                "toplamUcusSuresi":"0sa 0dk"
         });
-
+        const response_parsed = JSON.parse(response.config.data)
+    
         const updatedOrders = [
             ...orders,
-            response.data
+            response_parsed
             ];
-
         setOrders(updatedOrders);
     }
 
@@ -123,7 +166,7 @@ function App(){
         <Toast ref={toast} />
         <ConfirmDialog />
         <TabMenu model={menuItems} className="tabMenu" activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)}/>
-        <MainContent activeIndex={activeIndex} onEdit={editOrder} orders={orders} onDelete={deleteOrder} onCreate={createOrder} />
+        <MainContent activeIndex={activeIndex} onEdit={editOrder} orders={orders} musteriler={musteriler} musterilerTable={musterilerTable} ucakTipleri={ucakTipleri} ucakTipleriTable={ucakTipleriTable} lokasyonlar={lokasyonlar} lokasyonlarTable={lokasyonlarTable} onDelete={deleteOrder} onCreate={createOrder} toast={toast} />
         {/* <Orders onEdit={editOrder} orders={orders} onDelete={deleteOrder} />
         <Ucuslar onEdit={editOrder} orders={orders} onDelete={deleteOrder} /> */}
     </div>
